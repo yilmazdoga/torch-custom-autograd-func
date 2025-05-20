308e87c4ac929b1939c33e3387b3f3d8d7ac5a46
yilmazdoga <yilmaz.doga@gmail.com>
2025-05-20 13:31:48 +0100

    added batch learning with manual gradient accumulation

--- train.py
+++ train.py
@@ -1,9 +1,21 @@
 import torch
+import random
+import numpy as np
 from compute_func import compute
 from params import Params
 
+seed = 18
+torch.manual_seed(seed)
+random.seed(seed)
+np.random.seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
 sample_size = 5000
 num_epochs = 100
+batch_size = 10
 
 x = torch.randn(sample_size, 5, 1)
 y = torch.randn(sample_size, 3, 1)
@@ -12,17 +24,47 @@ mse_loss = torch.nn.MSELoss()
 params = Params()
 optimizer = params.optimizer
 
+mode = 'gradient_accum'
+
 for epoch in range(num_epochs):
-    for i in range(sample_size):
-        x_sample = x[i]
-        y_sample = y[i]
+    for i in range(sample_size // batch_size):
+        x_sample = x[i * batch_size: (i + 1) * batch_size]
+        y_sample = y[i * batch_size: (i + 1) * batch_size]
+        if mode == 'gradient_accum':
+            batch_grads_a = []
+            batch_grads_b = []
+            batch_grads_w = []
+            for x_item, y_item in zip(x_sample, y_sample):
+                out_y = compute(x_item, params)
+                final_loss = mse_loss(out_y, y_item)
+                final_loss.backward()
+                batch_grads_a.append(params.get_a.grad)
+                batch_grads_b.append(params.get_b.grad)
+                batch_grads_w.append(params.get_w.grad)
+            
+            batch_grads_a = torch.stack(batch_grads_a)
+            batch_grads_a = torch.mean(batch_grads_a, dim=0)
+            batch_grads_b = torch.stack(batch_grads_b)
+            batch_grads_b = torch.mean(batch_grads_b, dim=0)
+            batch_grads_w = torch.stack(batch_grads_w)
+            batch_grads_w = torch.mean(batch_grads_w, dim=0)
 
-        out_y = compute(x_sample, params)
+            params.get_a.grad = batch_grads_a
+            params.get_b.grad = batch_grads_b
+            params.get_w.grad = batch_grads_w
 
-        loss = mse_loss(out_y, y_sample)
+        elif mode == 'loss_accum':
+            batch_loss = 0.0
+            for x_item, y_item in zip(x_sample, y_sample):
+                out_y = compute(x_item, params)
+                loss = mse_loss(out_y, y_item)
+                batch_loss += loss
+            final_loss = batch_loss / batch_size
+            final_loss.backward()
+        else:
+            exit(1)
 
-        loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {final_loss.item():.4f}")
